cc6b53d7d817f3e995f1e3950d8d43de2ec1ed86
Renato Uchoa <renatoubra@gmail.com>
2017-10-09 22:59:12 -0300

    Inclusão de arquivo de templates para o PaginatorHelper
    Signed-off-by: Renato Uchoa <renatoubra@gmail.com>

---
+++ config/paginator.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'nextActive' => '<li><a href="{{url}}">{{text}}</a></li>',
+    'nextDisabled' => '<li class="disabled"><a>{{text}}</a></li>',
+    'prevActive' => '<li><a href="{{url}}">{{text}}</a></li>',
+    'prevDisabled' => '<li class="disabled"><a>{{text}}</a></li>',
+    'counterRange' => '{{start}} - {{end}} of {{count}}',
+    'counterPages' => '{{page}} of {{pages}}',
+    'first' => '<li><a href="{{url}}">{{text}}</a></li>',
+    'last' => '<li><a href="{{url}}">{{text}}</a></li>',
+    'number' => '<li><a href="{{url}}">{{text}}</a></li>',
+    'current' => '<li class="active"><a href="{{url}}">{{text}}</a></li>',
+    'ellipsis' => '<li class="ellipsis disabled"><a>...</a></li>',
+    'sort' => '<a href="{{url}}">{{text}}</a>',
+    'sortAsc' => '<a class="asc" href="{{url}}">{{text}}</a>',
+    'sortDesc' => '<a class="desc" href="{{url}}">{{text}}</a>',
+    'sortAscLocked' => '<a class="asc locked" href="{{url}}">{{text}}</a>',
+    'sortDescLocked' => '<a class="desc locked" href="{{url}}">{{text}}</a>',    
+]; 
