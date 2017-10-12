e60c4addcf0cd28708e02a85d8be92f35b7d5114
Renato Uchoa <renatoubra@gmail.com>
2017-10-12 11:36:20 -0300

    Implementação de action no controller EscolasController
    - Action: `identificacaoVisualizar`.
    
    Signed-off-by: Renato Uchoa <renatoubra@gmail.com>

--- src/Controller/EscolasController.php
+++ src/Controller/EscolasController.php
@@ -2,16 +2,30 @@
 
 namespace GRE\Controller;
 
+use Cake\Datasource\Exception\RecordNotFoundException;
+
 class EscolasController extends AppController
 {
     public function index()
     {
         return $this->redirect(['action' => 'listar']);
     }
-    
+
     public function listar()
     {
         $escolas = $this->paginate($this->Escolas->listar(['search' => $this->request->query]));
         $this->set(compact('escolas'));
     }
+
+    public function identificacaoVisualizar($escolaId = null)
+    {
+        try {
+            $escola = $this->Escolas->getIdentificacao($escolaId);
+            $this->set(compact('escola'));
+        } catch (RecordNotFoundException $e) {
+            $this->Flash->error('Escola inválida!');
+            return $this->redirect(['action' => 'listar']);
+        }
+    }
+
 }
