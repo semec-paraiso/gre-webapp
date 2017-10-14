736e71a618da8673ccf02f0f65582321f83aa43a
Renato Uchoa <renatoubra@gmail.com>
2017-10-14 10:53:39 -0300

    Melhorias no repositório MunicipiosTable
    - Implementação do método `listar()`
    
    Signed-off-by: Renato Uchoa <renatoubra@gmail.com>

--- src/Model/Table/MunicipiosTable.php
+++ src/Model/Table/MunicipiosTable.php
@@ -2,12 +2,41 @@
 
 namespace GRE\Model\Table;
 
+use Cake\ORM\Query;
+
+/**
+ * Repositório da entidade Municipios
+ * 
+ */
 class MunicipiosTable extends Table
 {
+    /**
+     * Instruções de inicialização
+     * 
+     * @param array $config
+     */
     public function initialize(array $config)
     {
         parent::initialize($config);
         
+        $this->setDisplayField('nome');
+
         $this->belongsTo('Ufs');
+    }
+
+    /**
+     * Obtém a relação de municípios cadastrados
+     * 
+     * @param array $options
+     * @return Query
+     */
+    public function listar(array $options = array()) : Query
+    {
+        $defaultOptions = [
+            'order' => 'Municipios.nome ASC',
+        ];
+        $options = array_merge($defaultOptions, $options);
+        
+        return $this->find('all', $options);
     }
 }
