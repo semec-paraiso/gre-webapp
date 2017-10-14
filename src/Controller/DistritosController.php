2eafbeccd59023b8c5f400df425a144acf23e880
Renato Uchoa <renatoubra@gmail.com>
2017-10-14 11:04:09 -0300

    Implementação do controller DistritosController
    Signed-off-by: Renato Uchoa <renatoubra@gmail.com>

---
+++ src/Controller/DistritosController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace GRE\Controller;
+
+/**
+ * Controller Distritos
+ * 
+ */
+class DistritosController extends AppController
+{
+    /**
+     * Lista os distritos cadastrados
+     * Devolve uma resposta em JSON caso a requisição seja do tipo ajax.
+     * 
+     * @param int $municipioId
+     * @return void
+     */
+    public function listar($municipioId = null)
+    {
+        if ($this->request->is('ajax')) {
+            return $this->_ajaxListar($municipioId);
+        }
+    }
+    
+    /**
+     * Prepara a resposta para a requisição ajax
+     * 
+     * @param int $municipioId
+     */
+    protected function _ajaxListar($municipioId)
+    {
+        $this->viewBuilder()->setLayout('ajax');
+        $this->viewBuilder()->setTemplate('ajax_listar');
+        $distritos = $this->Distritos->listar(['conditions' => ['municipio_id' => $municipioId]]);
+        $this->set(compact('distritos'));
+    }
+}
