ee29ea2f384b502db5c0f7127f0593a0a203f9ad
Renato Uchoa <renatoubra@gmail.com>
2017-10-29 14:11:38 -0300

    Melhorias no repositório Reconhecimentos
    - Acréscimo de regras de validação;
    - Conversão da data de vencimento
    
    Signed-off-by: Renato Uchoa <renatoubra@gmail.com>

--- src/Model/Table/ReconhecimentosTable.php
+++ src/Model/Table/ReconhecimentosTable.php
@@ -2,6 +2,11 @@
 
 namespace GRE\Model\Table;
 
+use ArrayObject;
+use Cake\Event\Event;
+use Cake\Validation\Validator;
+use GRE\Formatting\Date;
+
 /**
  * Repositório `Reconhecimentos`
  * 
@@ -18,5 +23,42 @@ class ReconhecimentosTable extends Table
         parent::initialize($config);
         
         $this->setEntityClass('Reconhecimento');
+    }
+    
+    /**
+     * Regras de validação default
+     * 
+     * @param Validator $validator
+     * @return Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator->requirePresence('curso', 'create', 'Informe o curso');
+        $validator->requirePresence('ato', 'create', 'Informe o ato de reconhecimento');
+        $validator->requirePresence('validade', 'create', 'Informe a data de validade do ato');
+        
+        $validator->notEmpty('curso',  'Informe o curso');
+        $validator->notEmpty('ato', 'Informe o ato de reconhecimento');
+        $validator->notEmpty('validade', 'Informe a data de validade');
+        
+        $validator->date('validade', 'ymd', 'Informe uma data válida');
+        
+        return $validator;
+    }
+    
+    /**
+     * Conversão de valores a serem salvos
+     * 
+     * @param Event $event
+     * @param ArrayObject $data
+     * @param ArrayObject $options
+     * @return ArrayObject
+     */
+    public function beforeMarshal(Event $event, ArrayObject $data, ArrayObject $options)
+    {
+        if (isset($data['validade'])) {
+            $data['validade'] = Date::brToPhp($data['validade']);
+        }
+        return $data;
     }
 }
