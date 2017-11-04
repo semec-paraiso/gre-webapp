932e788af7b80387d62515c5bcab2f55ede6a5d3
Renato Uchoa <renatoubra@gmail.com>
2017-11-04 17:12:51 -0300

    Refatoração do repositório base
    - Remoção de métodos não utilizados
    - Implementação do método getFilters()
    
    Signed-off-by: Renato Uchoa <renatoubra@gmail.com>

--- src/Model/Table/Table.php
+++ src/Model/Table/Table.php
@@ -10,45 +10,39 @@ use Cake\ORM\Query;
  */
 class Table extends \Cake\ORM\Table
 {
+    /**
+     * Campos possíveis de serem filtrados nas buscas
+     * 
+     * Utilizar o formato:
+     * array(
+     *     'campo1' => 'valorDefault',
+     *     'campo2' => 'valorDefault',
+     *     // ...
+     * )
+     *
+     * @var array
+     */
+    protected $_filters = [];
+    
+    /**
+     * Instruções de inicialização de todos os repositórios
+     * 
+     * @param array $config
+     */
     public function initialize(array $config)
     {
         parent::initialize($config);
         
         $this->addBehavior('Timestamp');
     }
+ 
     /**
-     * Filtra uma query
+     * Obtém os campos para filtragem e seus valores default
      * 
-     * @param Query $query Query a ser filtrada
-     * @param string $field Campo a ser aplicado o filtro
-     * @param string $search Fragmento do texto a ser buscado
-     * 
-     * @return Query
-     */
-    protected function _filterResult(Query $query, string $field, string $search) : Query
-    {
-        $query->where([$field . ' LIKE' => '%' . trim($search) .'%']);
-        return $query;
-    }
-    
-    /**
-     * Retorna o array `$data` apenas com os conjuntos de chave/valor cujas
-     * chaves estejam no array `$keys`
-     * 
-     * @param array $data
-     * @param array $keys
      * @return array
      */
-    protected function _filterData(array $data, array $keys) : array
+    public function getFilters()
     {
-        $filtered = [];
-        
-        foreach ($keys as $key) {
-            if (isset($data[$key])) {
-                $filtered[$key] = $data[$key];
-            }
-        }
-        
-        return $filtered;
+        return $this->_filters;
     }
 }
