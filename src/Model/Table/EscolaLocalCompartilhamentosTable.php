64d0a53c685f597cd2572b781ab9ca5ea5c10898
Renato Uchoa <renatoubra@gmail.com>
2017-11-06 19:56:01 -0300

    Implementação inicial de regras de validação no repositório EscolaLocalCompartilhamentos
    Signed-off-by: Renato Uchoa <renatoubra@gmail.com>

--- src/Model/Table/EscolaLocalCompartilhamentosTable.php
+++ src/Model/Table/EscolaLocalCompartilhamentosTable.php
@@ -2,6 +2,8 @@
 
 namespace GRE\Model\Table;
 
+use Cake\Validation\Validator;
+
 /**
  * Repositório `EscolaLocalCompartilhamentos`
  */
@@ -27,5 +29,24 @@ class EscolaLocalCompartilhamentosTable extends Table
 
         $this->belongsTo('Escolas');
     }
-
+    
+    /**
+     * Regras de validação default
+     * 
+     * @param Validator $validator
+     * @return Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator->requirePresence('escola_local_id', 'create', 'Informe o local');
+        $validator->requirePresence('escola_id', 'create', 'Informe o local');
+        
+        $validator->notEmpty('escola_local_id', 'Informe o local');
+        $validator->notEmpty('escola_id', 'Informe a escola');
+        
+        $validator->integer('escola_local_id', 'Informe o local');
+        $validator->integer('escola_id', 'Informe a escola');
+        
+        return $validator;
+    }
 }
