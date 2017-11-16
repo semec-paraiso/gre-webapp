d85184588e2add85a0bd6e5a9415eb1067e2b7bd
Renato Uchoa <renatoubra@gmail.com>
2017-11-16 13:25:03 -0300

    Implementação inicial de testes para a classe GRE\View\Helper\ToolbarHelper
    Signed-off-by: Renato Uchoa <renatoubra@gmail.com>

--- src/View/Helper/ToolbarHelper.php
+++ src/View/Helper/ToolbarHelper.php
@@ -4,10 +4,24 @@ namespace GRE\View\Helper;
 
 use Cake\View\Helper;
 
+/**
+ * Helper para a construção de toolbars
+ */
 class ToolbarHelper extends Helper
 {
+    /**
+     * Outros helpers utilizados
+     *
+     * @var array
+     */
     public $helpers = ['Html', 'ButtonGroup'] ;
     
+    /**
+     * Constroi o HTML de uma toolbar
+     * 
+     * @param array $options
+     * @return string
+     */
     public function render(array $options = []) : string
     {
         $defaultOptions = [
