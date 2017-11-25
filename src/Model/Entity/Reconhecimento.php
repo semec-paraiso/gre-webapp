dcf03677d25d9f644cf34c4c7c2fcf78d725fddf
Renato Uchoa <renatoubra@gmail.com>
2017-11-25 15:20:48 -0300

    Refatoração da entidade Reconhecimento (#199)
    Signed-off-by: Renato Uchoa <renatoubra@gmail.com>

--- src/Model/Entity/Reconhecimento.php
+++ src/Model/Entity/Reconhecimento.php
@@ -11,6 +11,47 @@ use Cake\I18n\Date;
 class Reconhecimento extends Entity
 {
     /**
+     * Data de hoje
+     * Será utilizada para verificar se o reconhecimento está vencido
+     *
+     * @var Date
+     */
+    protected $_today;
+    
+    /**
+     * Construtor
+     * Detine a data de hoje automaticamente
+     * 
+     * @param array $properties
+     * @param array $options
+     */
+    public function __construct(array $properties = array(), array $options = array())
+    {
+        parent::__construct($properties, $options);
+        $this->setToday(Date::today());
+    }
+    
+    /**
+     * Define a data de hoje
+     * 
+     * @param Date $today
+     */
+    public function setToday(Date $today)
+    {
+        $this->_today = $today;
+    }
+    
+    /**
+     * Obtém a data de hoje definida na entidade
+     * 
+     * @return Date
+     */
+    public function getToday() : Date
+    {
+        return $this->_today;
+    }
+    
+    /**
      * Verifica se a data de vencimento do Reconhecimento é menor que a data
      * atual definida no sistema
      * 
@@ -19,8 +60,8 @@ class Reconhecimento extends Entity
     public function vencido() : bool
     {
         if (!$this->validade) {
-            return true;
-        }
-        return Date::today()->gt($this->validade);
+            return true; 
+        } 
+        return $this->getToday()->gt($this->validade); 
     }
 }
